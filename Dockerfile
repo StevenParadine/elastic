cf8fc8caa870ee59275dbbc1796a429eadd14c0d
Steven Paradine <steven.paradine@canada.ca>
2019-02-05 14:32:53 -0500

    Newer branch
    resources/elastic_search

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,14 @@
-FROM elasticsearch:1.6
+FROM docker.elastic.co/elasticsearch/elasticsearch-oss:6.2.4
 
 MAINTAINER Justin Smith <jjsmith@agiledigital.com.au>
 
-RUN plugin --install mobz/elasticsearch-head && plugin -install karmi/elasticsearch-paramedic && plugin -install lukas-vlcek/bigdesk/2.5.0 && plugin --install elasticsearch/elasticsearch-cloud-aws/2.6.1
-
-RUN plugin --install statsd -url https://github.com/Automattic/elasticsearch-statsd-plugin/releases/download/v0.4.0/elasticsearch-statsd-0.4.0.zip
+RUN ./bin/elasticsearch-plugin install repository-s3
+RUN ./bin/elasticsearch-plugin install https://github.com/Automattic/elasticsearch-statsd-plugin/releases/download/6.2.4.0/elasticsearch-statsd-6.2.4.0.zip
 
 COPY start.sh /opt/bin/start.sh
 
 COPY elasticsearch.yml /usr/share/elasticsearch/config/
 
-COPY patent_listings_mapping.json /opt/data/patent_listings_mapping.json
-
-COPY patent_template_mapping.json /opt/data/patent_template_mapping.json
-
-COPY filing_template_mapping.json /opt/data/filing_template_mapping.json
+COPY es_*.json /opt/data/
 
 ENTRYPOINT ["/opt/bin/start.sh"]
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 function wait_for_server() {
-  until [ `curl -s -o /dev/null -w "%{http_code}" "http://localhost:9200" | grep -cim1 "200"` -ge 1 ]; do
+  until [ `curl --silent --output /dev/null --write-out "%{http_code}" "http://localhost:9200" | grep -cim1 "200"` -ge 1 ]; do
     sleep 1
   done
 }
@@ -13,7 +13,7 @@ then
   CLUSTER_NAME="source_ip_${ENVIRONMENT}"
 else
   CLUSTER_NAME="source_ip_development"
-  echo "ENVIRONMENT is not set, you should set this in elastic search and public-api to the same value to isolate the cluster. Using [${CLUSTER_NAME}] as default cluster name."
+  echo "ENVIRONMENT is not set, you should set this in elasticsearch and public-api to the same value to isolate the cluster. Using [${CLUSTER_NAME}] as default cluster name."
 fi
 
 echo "Setting Cluster Name to [${CLUSTER_NAME}]"
@@ -64,7 +64,7 @@ else
     echo "STATSD_HOST set based on docker-compose links to [${STATSD_HOST}]."
   else
     STATSD_HOST="localhost"
-    echo "STATSD_HOST is not set, you will not receive any metrics from elastic search. Using [${STATSD_HOST}] as default host."
+    echo "STATSD_HOST is not set, you will not receive any metrics from elasticsearch. Using [${STATSD_HOST}] as default host."
   fi
 fi
 export STATSD_HOST
@@ -91,63 +91,119 @@ then
   export STATSD_UPDATE_EVERY
 fi
 
+# Create the keystore and add the AWS credentials into it.
+# The keystore must be created before starting elasticsearch, otherwise changes will not be registered until ES restarts
+echo "Creating elasticsearch keystore"
+bin/elasticsearch-keystore create
 
-elasticsearch &
+echo "Configuring AWS credentials"
+# These credentials will come in from secrets from openshift
+# In the staging environment this will be readonly for restoring live backups.
+# In the live environment this will be write access for creating backups.
+# --force is used to ensure the value is overridden from environment.
+echo ${S3_CLIENT_ACCESS_KEY} | bin/elasticsearch-keystore add --stdin --force s3.client.s3_backup.access_key
+echo ${S3_CLIENT_SECRET_KEY} | bin/elasticsearch-keystore add --stdin --force s3.client.s3_backup.secret_key
 
-echo "Waiting for elastic search..."
+/usr/local/bin/docker-entrypoint.sh &
+
+echo "Waiting for elasticsearch..."
 
 wait_for_server
 
 echo "Server available"
 
-if [ `curl -s -o /dev/null -w "%{http_code}" "http://localhost:9200/patent_listings" | grep -cim1 "404"` -ge 1 ]
+# Now that the key store is setup and configured, add the s3 repository to elasticsearch.
+echo "Configuring S3 repository for backups"
+curl -XPUT 'http://localhost:9200/_snapshot/s3_backup' --header "Content-Type: application/json" -d '{
+  "type": "s3",
+  "settings": {
+    "client": "s3_backup",
+    "bucket": "sourceip-automated-backups",
+    "base_path": "elasticsearch"
+  }
+}'
+
+
+# Add in the elasticsearch indexes we need for the SourceIP project.
+if [ `curl --silent --output /dev/null --write-out "%{http_code}" "http://localhost:9200/_template/all" | grep -cim1 "404"` -ge 1 ]
 then
-  echo "Patent listings index not found, creating"
-  curl -XPUT "http://localhost:9200/patent_listings" --upload-file /opt/data/patent_listings_mapping.json
+  echo "Global template not found, creating"
+  curl --request PUT --header "Content-Type: application/json" --upload-file /opt/data/es_template_all.json "http://localhost:9200/_template/all"
 fi
 
-if [ `curl -s -o /dev/null -w "%{http_code}" "http://localhost:9200/_template/patents" | grep -cim1 "404"` -ge 1 ]
+if [ `curl --silent --output /dev/null --write-out "%{http_code}" "http://localhost:9200/_template/patents" | grep -cim1 "404"` -ge 1 ]
 then
   echo "Patents template not found, creating"
-  curl -XPUT "http://localhost:9200/_template/patents" --upload-file /opt/data/patent_template_mapping.json
+  curl --request PUT --header "Content-Type: application/json" --upload-file /opt/data/es_template_patents.json "http://localhost:9200/_template/patents"
 fi
 
-if [ `curl -s -o /dev/null -w "%{http_code}" "http://localhost:9200/_template/filings" | grep -cim1 "404"` -ge 1 ]
+if [ `curl --silent --output /dev/null --write-out "%{http_code}" "http://localhost:9200/_template/filings" | grep -cim1 "404"` -ge 1 ]
 then
   echo "Raw filing data template not found, creating"
-  curl -XPUT "http://localhost:9200/_template/filings" --upload-file /opt/data/filing_template_mapping.json
+  curl --request PUT --header "Content-Type: application/json" --upload-file /opt/data/es_template_filings.json "http://localhost:9200/_template/filings"
 fi
 
-if [ `curl -s -o /dev/null -w "%{http_code}" "http://localhost:9200/patents-dawn" | grep -cim1 "404"` -ge 1 ]
+if [ `curl --silent --output /dev/null --write-out "%{http_code}" "http://localhost:9200/patent_listings" | grep -cim1 "404"` -ge 1 ]
+then
+  echo "Patent listings index not found, creating"
+  curl --request PUT --header "Content-Type: application/json" --upload-file /opt/data/es_index_patent_listings.json "http://localhost:9200/patent_listings"
+fi
+
+if [ `curl --silent --output /dev/null --write-out "%{http_code}" "http://localhost:9200/patent_listings_global" | grep -cim1 "404"` -ge 1 ]
+then
+  echo "Global patent listings index not found, creating"
+  curl --request PUT --header "Content-Type: application/json" --upload-file /opt/data/es_index_patent_listings_global.json "http://localhost:9200/patent_listings_global"
+fi
+
+# A common pattern here is to create two structurally-identical indices, *-dawn and *-recent.
+# This is purely to improve recovery time in case of node failure/crash.
+# A heuristic is used to choose which index a filing belongs in, based on its filing ID string.
+# * It creates a deterministic mapping from filing ID to index.
+# * It tends to place "old" patents in the *-dawn index, and "new" patents in the *-recent index,
+#   but it is not guaranteed to be that way.
+
+# Indices for data from AusPat, IP Australia's patent database.
+if [ `curl --silent --output /dev/null --write-out "%{http_code}" "http://localhost:9200/patents-dawn" | grep -cim1 "404"` -ge 1 ]
 then
   echo "patents-dawn index not found, creating"
-  curl -XPUT "http://localhost:9200/patents-dawn" # Will be based on patents template.
+  curl --request PUT "http://localhost:9200/patents-dawn" # Will be based on patents template.
 fi
 
-if [ `curl -s -o /dev/null -w "%{http_code}" "http://localhost:9200/patents-recent" | grep -cim1 "404"` -ge 1 ]
+if [ `curl --silent --output /dev/null --write-out "%{http_code}" "http://localhost:9200/patents-recent" | grep -cim1 "404"` -ge 1 ]
 then
   echo "patents-recent index not found, creating"
-  curl -XPUT "http://localhost:9200/patents-recent" # Will be based on patents template.
+  curl --request PUT "http://localhost:9200/patents-recent" # Will be based on patents template.
 fi
 
-if [ `curl -s -o /dev/null -w "%{http_code}" "http://localhost:9200/patents-manual" | grep -cim1 "404"` -ge 1 ]
+# Index for manually-added patents, structurally identical to the AusPat indices.
+if [ `curl --silent --output /dev/null --write-out "%{http_code}" "http://localhost:9200/patents-manual" | grep -cim1 "404"` -ge 1 ]
 then
   echo "patents-manual index not found, creating"
-  curl -XPUT "http://localhost:9200/patents-manual" # Will be based on patents template.
+  curl --request PUT "http://localhost:9200/patents-manual" # Will be based on patents template.
 fi
 
-if [ `curl -s -o /dev/null -w "%{http_code}" "http://localhost:9200/filings-wipo-dawn" | grep -cim1 "404"` -ge 1 ]
+# Indices for data from PATENTSCOPE, WIPO's patent database.
+if [ `curl --silent --output /dev/null --write-out "%{http_code}" "http://localhost:9200/filings-wipo-dawn" | grep -cim1 "404"` -ge 1 ]
 then
   echo "filings-wipo-dawn index not found, creating"
-  curl -XPUT "http://localhost:9200/filings-wipo-dawn-v1" # Will be based on filing template.
-  curl -XPOST "http://localhost:9200/_aliases" -d '{ "actions": [ { "add": { "index": "filings-wipo-dawn-v1", "alias": "filings-wipo-dawn" } } ] }'
+  curl --request PUT "http://localhost:9200/filings-wipo-dawn-v1" # Will be based on filings template.
+  curl --request PUT "http://localhost:9200/filings-wipo-dawn-v1/_alias/filings-wipo-dawn"
 fi
 
-if [ `curl -s -o /dev/null -w "%{http_code}" "http://localhost:9200/filings-wipo-recent" | grep -cim1 "404"` -ge 1 ]
+if [ `curl --silent --output /dev/null --write-out "%{http_code}" "http://localhost:9200/filings-wipo-recent" | grep -cim1 "404"` -ge 1 ]
 then
   echo "filings-wipo-recent index not found, creating"
-  curl -XPUT "http://localhost:9200/filings-wipo-recent-v1" # Will be based on filing template.
-  curl -XPOST "http://localhost:9200/_aliases" -d '{ "actions": [ { "add": { "index": "filings-wipo-recent-v1", "alias": "filings-wipo-recent" } } ] }'
+  curl --request PUT "http://localhost:9200/filings-wipo-recent-v1" # Will be based on filings template.
+  curl --request PUT "http://localhost:9200/filings-wipo-recent-v1/_alias/filings-wipo-recent"
+fi
+
+# Indices for data from DOCDB, EPO's patent database.
+if [ `curl --silent --output /dev/null --write-out "%{http_code}" "http://localhost:9200/filings-docdb-all" | grep -cim1 "404"` -ge 1 ]
+then
+  echo "filings-docdb-all index not found, creating"
+  curl --request PUT "http://localhost:9200/filings-docdb-all-v1" # Will be based on filings template.
+  curl --request PUT "http://localhost:9200/filings-docdb-all-v1/_alias/filings-docdb-all"
 fi
 
 wait
+
